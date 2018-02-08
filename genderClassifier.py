de5b3690cd70a98ae5e42b7d53db2ac42892f751
anirudh3 <anirudh3@gmail.com>
2018-02-08 03:51:16 -0500

    adding SVM

--- genderClassifier.py
+++ genderClassifier.py
@@ -11,11 +11,8 @@ import sys, os
 import numpy as np
 from getFeatures import SpeechFeatures # our feature extractor
 from matplotlib import pyplot as plt
-
-
-
-# declaring globally useful variables
-
+from sklearn.svm import SVC
+import random
 
 
 
@@ -24,25 +21,55 @@ from matplotlib import pyplot as plt
 features = SpeechFeatures()
 features.load_data()
 
-# m = [v for v in features.men_pitches[k] for k in features.men_pitches.keys()]
-# f = [features.women_pitches[k] for k in features.women_pitches.keys()]
-m = []
+m = [] # holds the non zero pitch values for men
 for k in features.men_pitches.keys():
     temp = [v for v in features.men_pitches[k] if v > 0.0]
     m.extend(temp)
 
-w = []
+w = [] # holds the non zero pitch values for women
 for k in features.women_pitches.keys():
     temp = [v for v in features.women_pitches[k] if v > 0.0]
     w.extend(temp)
 
 
-mean_m = sum(m)/len(m)
-mean_w = sum(w)/len(w)
+# Preparing input for the classifier
+inp_train = []
+label_train = []
+inp_test = []
+label_test = []
+
+# 80% train set, 20% test set
+random.shuffle(m)
+random.shuffle(w)
 
 
-print m
+for v in m[:int(len(m) * 0.8)]:
+    inp_train.append([v])
+    label_train.append(1)
 
-plt.plot(m, 'go', w, 'rs')
-plt.ylabel('pitch')
-plt.show()
+for v in w[:int(len(w) * 0.8)]:
+    inp_train.append([v])
+    label_train.append(2)
+
+for v in m[int(len(m) * 0.8):]:
+    inp_test.append([v])
+    label_test.append(1)
+
+for v in w[int(len(w) * 0.8):]:
+    inp_test.append([v])
+    label_test.append(2)
+
+# classifying using SVM
+
+clf = SVC(kernel='poly', verbose=True)
+clf.fit(inp_train, label_train)
+
+print clf.score(inp_test, label_test)
+
+
+
+
+
+# plt.plot(m, 'go', w, 'rs')
+# plt.ylabel('pitch')
+# plt.show()
