fb92534062f3db1ffd7723c2434ff4c79af36bd8
anirudh3 <anirudh3@gmail.com>
2018-02-08 04:18:55 -0500

    few pythonic fixes

--- genderClassifier.py
+++ genderClassifier.py
@@ -61,7 +61,7 @@ for v in w[int(len(w) * 0.8):]:
 
 # classifying using SVM
 
-clf = SVC(kernel='poly', verbose=True)
+clf = SVC(kernel='poly', degree=2, verbose=True)
 clf.fit(inp_train, label_train)
 
 print clf.score(inp_test, label_test)
